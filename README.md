60f5f1e076af6f262de4e7f1ffad455b0fa08f5d
maxsupermanhd <q3.max.2011@ya.ru>
2020-11-08 13:56:24 +0300

    Add license notices and comments

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cterminal
 Customizable IO wrapper for multithreaded applications.
+Uses pthread, build with `-lpthread`.
+
+# Example
+See main.c
--- main.c
+++ main.c
@@ -1,14 +1,34 @@
+/*
+	This file is part of cterminal.
+
+    cterminal is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    cterminal is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with cterminal.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include "cterminal.h"
 
+// exit flag
 bool needexit = false;
 pthread_mutex_t needexitmutex = PTHREAD_MUTEX_INITIALIZER;
 
+// commander function, recieves null-terminated commands
+// called with pthread_create inside cterminal
 void commdr(char* command) {
 	if(strcmp(command, "lolkekexitme") == 0) {
+		CTprintf("Exit scheduled.");
 		pthread_mutex_lock(&needexitmutex);
 		needexit = 1;
 		pthread_mutex_unlock(&needexitmutex);
@@ -16,6 +36,7 @@ void commdr(char* command) {
 	return;
 }
 
+// simple spammer example, will not respond to exit flag until finished work (sleep)
 void *WorkerThreadFunction(void*) {
 	for(int i=0; ; i++) {
 		usleep(rand()%900000);
@@ -30,10 +51,17 @@ void *WorkerThreadFunction(void*) {
 }
 
 int main() {
+	// init
 	CTinit(&commdr);
+
+	// create workload
 	pthread_t WorkerThread;
 	pthread_create(&WorkerThread, NULL, &WorkerThreadFunction, NULL);
+
+	// wait till we are ready to exit
 	pthread_join(WorkerThread, NULL);
+
+	// shutdown & cleanup
 	CTexit();
 	return 0;
 }
